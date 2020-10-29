87b679df7bb9c69475548dcb9faab50911ae69c4
Tausifqureshi786 <59664677+Tausifqureshi786@users.noreply.github.com>
2020-10-29 02:17:52 -0600

    created the editor in react using CKE library and tried to implement web sockets on server and encountered a cors error and the data is not going througn from the client to the server and the communication hasn't happen yet.

--- editor/src/App.js
+++ editor/src/App.js
@@ -1,11 +1,48 @@
 import './App.css';
+import 'materialize-css/dist/css/materialize.min.css';
+import React, { useState} from 'react';
+import 'socket.io';
+import io from 'socket.io-client';
+import ReactHtmlParser from 'react-html-parser';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import  './server';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
-function App() {
-  return (
-    <div className="App">
-      <p> This is our new create react app </p>
+
+// import M from  'materialize-css/dist/js/materialize.min.js';
+// import Navbar from "./Navbar.js"; 
+
+const socket = io('http://localhost:3002');
+
+function App ()  {
+
+  const [value, setValue] = useState("") ;
+
+   const handleOnChange = (event , editor) => {
+    var data = editor.getData();
+    setValue(data);
+    socket.send(data);
+    
+    
+    }
+  
+
+  return (  
+
+    <div className="text" > 
+          <CKEditor 
+          editor={ ClassicEditor }
+          onChange={  handleOnChange  }/>
+          
+          
+      <div> 
+        {ReactHtmlParser(value)}
+        </div>
     </div>
+
+   
   );
+
 }
 
-export default App;
+export default App
---
+++ editor/src/server.js
@@ -0,0 +1,22 @@
+
+
+
+// const http = require('http').createServer();
+// const io = require('socket.io')(http, { origins: '*:*'});
+
+// const port = 8080;
+
+// http.listen(port, () => console.log(`server listening on port: ${port}`))
+
+//  io.on('connection', (socket) => {
+//     console.log('connected')
+    
+//     socket.on('message', (evt) => {
+//         console.log(evt)
+//         socket.broadcast.emit('message', evt)
+//     })
+// })
+
+// io.on('disconnect', (evt) => {
+//     console.log('some people left')
+// })
